7580dfebf49564736b8fddf76f7ee339049fc28c
esimms999-gsk <Eric.M.Simms@gsk.com>
2023-07-21 13:28:11 +0000

    Temprary testing only.

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -18,8 +18,8 @@
 #' hello_admiral(hw = FALSE)
 hello_admiral <- function(hw = TRUE) {
   if (hw) {
-    message("Eric, welcome to the admiral family!")
+    message("Hello Eric, welcome to the admiral family!")
   } else {
-    message("Eric, welcome to the admiral family!")
+    message("Hello Eric, welcome to the admiral family!")
   }
 }
--- tests/testthat/testthat/test-my_first_fcn.R
+++ tests/testthat/testthat/test-my_first_fcn.R
@@ -1,13 +1,13 @@
 test_that("hello admiral greets without hw", {
   expect_message(
     hello_admiral(),
-    "^Eric, welcome to the admiral family!\\n"
+    "^Hello Eric, welcome to the admiral family!\\n"
   )
 })
 
 test_that("hello admiral greets with hw", {
   expect_message(
     hello_admiral(TRUE),
-    "^Eric, welcome to the admiral family!\\n"
+    "^Hello Eric, welcome to the admiral family!\\n"
   )
 })
